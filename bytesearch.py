4000b1a7a5ba4684e29ab588bd861ff2c85a7c3c
Michael Olson <olsonmichael32@gmail.com>
2024-10-27 20:41:48 -0400

    make it easier to add pattern

--- bytesearch.py
+++ bytesearch.py
@@ -13,7 +13,8 @@ def main():
 
   #last iteration?
   packedfilter.search_s_p.argtypes = [POINTER(c_ubyte), c_int, POINTER(c_ubyte), c_int]
-  hex_string = "4C 8B 51 18 4D 8B D8 48 8B 05 ?? ?? ?? ?? 4D 8D 42 18 49 39 40 08 75 1C 48 8B 05 ?? ?? ?? ?? 49 39 00 75 10 0F 10 02 4D 89 5A 28 45 88 4A 30 41 0F 11 00 C3 48 8D 0D ?? ?? ?? ?? E9 ?? ?? ?? ??".replace(" ", "")
+  string_to_find = "4C 8B 51 18 4D 8B D8 48 8B 05 ?? ?? ?? ?? 4D 8D 42 18 49 39 40 08 75 1C 48 8B 05 ?? ?? ?? ?? 49 39 00 75 10 0F 10 02 4D 89 5A 28 45 88 4A 30 41 0F 11 00 C3 48 8D 0D ?? ?? ?? ?? E9 ?? ?? ?? ??"
+  hex_string = string_to_find.replace(" ", "")
   patt_len = len(hex_string)
   b_string = hex_string.encode('utf-8')
   P = (c_ubyte * patt_len).from_buffer_copy(bytearray(b_string))
